d242cec7b4d237d4bf35662c1c793450f1d2fa80
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2020-12-02 23:00:21 +0100

    whois command based on verification json

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,3 +1,4 @@
+import config_vars
 import collections
 import string
 import urllib.parse
@@ -8,66 +9,99 @@ from discord.ext import commands
 
 # This can be thought of as a miscellaneous category (anything 'utility' based.)
 
+
 class Utility(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['char'])
+    def get_verifications(self, user):
+        verifications = {}
+        try:
+            with open(config_vars.verifications_json_path, "r") as f:
+                jsonStates = json.loads(f.read())
+                for verifier_id, verified_sets in jsonStates.items():
+                    verified_set = next(
+                        (verified_set for verified_set in verified_sets if verified_set['user'] == user.id), None)
+                    if verified_set:
+                        verifications[self.bot.get_user(int(
+                            verifier_id))] = verified_set['at']
+        except FileNotFoundError:
+            print(
+                f'verifications.json not found at {config_vars.verifications_json_path}')
+        return verifications
+
+    @ commands.command(aliases=['who'])
+    async def whois(self, ctx, user_name):
+        user = next((user for user in ctx.guild.members if user.name.lower(
+        ) == user_name.lower()), None)
+        if user:
+            verifications = self.get_verifications(user)
+            if verifications:
+                await ctx.send(f'`{user}`. History:')
+                for verifier, at in verifications.items():
+                    await ctx.send(
+                        f'{at.split(".")[0]}: Verified by `{verifier}`')
+            else:
+                await ctx.send(f'I have no history on `{user}`. They were either here before me or was added directly by an admin.')
+
+    @ commands.command(aliases=['char'])
     async def characters(self, ctx, string):
         await ctx.send(len(string))
 
-    @commands.command(aliases=['wc'])
+    @ commands.command(aliases=['wc'])
     async def wordcount(self, ctx, *args):
         await ctx.send(len(args))
 
-    @commands.command(aliases=['rev'])
+    @ commands.command(aliases=['rev'])
     async def reverse(self, ctx, message):
-        await ctx.send(message[::(- 1)])
+        await ctx.send(message[:: (- 1)])
 
-    @commands.command()
+    @ commands.command()
     async def counteach(self, ctx, message):
         # Count the amount of characters in a string.
         count = {}
-        
+
         for char in message:
             if char in count.keys():
                 count[char] += 1
             else:
                 count[char] = 1
-        
+
         await ctx.send(str(count))
 
-    @commands.command(aliases=['head'])
+    @ commands.command(aliases=['head'])
     async def magicb(self, ctx, filetype):
         # Get the magic bytes from a filetype
         file = open('magic.json').read()
         alldata = json.loads(file)
         try:
             messy_signs = str(alldata[filetype]['signs'])
-            signs = messy_signs.split('[')[1].split(',')[0].split(']')[0].replace("'", '')
+            signs = messy_signs.split('[')[1].split(',')[0].split(']')[
+                0].replace("'", '')
             filetype = alldata[filetype]['mime']
             await ctx.send(f'''{filetype}: {signs}''')
-        except: # if the filetype is not in magicb.json...
+        except:  # if the filetype is not in magicb.json...
             await ctx.send(f"{filetype} not found :(  If you think this filetype should be included please do `>request \"magicb {filetype}\"`")
 
-    @commands.command()
+    @ commands.command()
     async def twitter(self, ctx, twituser):
         await ctx.send('https://twitter.com/' + twituser)
 
-    @commands.command()
+    @ commands.command()
     async def github(self, ctx, gituser):
         await ctx.send('https://github.com/' + gituser)
 
-    @commands.command(aliases=['5050', 'flip'])
+    @ commands.command(aliases=['5050', 'flip'])
     async def cointoss(self, ctx):
         choice = random.randint(1, 2)
-        
+
         if choice == 1:
             await ctx.send('heads')
-        
+
         if choice == 2:
             await ctx.send('tails')
+
 
 def setup(bot):
     bot.add_cog(Utility(bot))
--- nullctf.py
+++ nullctf.py
@@ -2,23 +2,28 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import os
+import re
 import sys
 
 import help_info
 import config_vars
 
-client = discord.Client()
-bot = commands.Bot(command_prefix=">")
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=">", intents=intents)
 # The default help command is removed so a custom one can be added.
 bot.remove_command('help')
 
 # Each extension corresponds to a file within the cogs directory.  Remove from the list to take away the functionality.
-extensions = ['ctf', 'ctftime', 'configuration', 'encoding', 'cipher', 'utility']
+extensions = ['ctf', 'ctftime', 'configuration',
+              'encoding', 'cipher', 'utility']
 # List of names reserved for those who gave cool ideas or reported something interesting.
-    # please don't spam me asking to be added.  if you send something interesting to me i will add you to the list.
+# please don't spam me asking to be added.  if you send something interesting to me i will add you to the list.
 # If your name is in the list and you use the command '>amicool' you'll get a nice message.
-cool_names = ['nullpxl', 'Yiggles', 'JohnHammond', 'voidUpdate', 'Michel Ney', 'theKidOfArcrania', 'l14ck3r0x01', 'hasu', 'KFBI', 'mrFu', 'warlock_rootx', 'd347h4ck', 'tourpan', 'careless_finch', 'fumenoid', '_wh1t3r0se_', 'The_Crazyman'] 
+cool_names = ['nullpxl', 'Yiggles', 'JohnHammond', 'voidUpdate', 'Michel Ney', 'theKidOfArcrania', 'l14ck3r0x01', 'hasu', 'KFBI',
+              'mrFu', 'warlock_rootx', 'd347h4ck', 'tourpan', 'careless_finch', 'fumenoid', '_wh1t3r0se_', 'The_Crazyman', 'hutli', 'zanderdk']
 # This is intended to be circumvented; the idea being that people will change their names to people in this list just so >amicool works for them, and I think that's funny.
+
 
 @bot.event
 async def on_ready():
@@ -27,6 +32,7 @@ async def on_ready():
     print("-------------------------------")
 
     await bot.change_presence(activity=discord.Game(name=">help | >source"))
+
 
 @bot.command()
 async def help(ctx, page=None):
@@ -43,7 +49,7 @@ async def help(ctx, page=None):
     elif page == 'utility':
         emb = discord.Embed(description=help_info.utility_help, colour=4387968)
         emb.set_author(name='Utilities Help')
-    
+
     else:
         emb = discord.Embed(description=help_info.help_page, colour=4387968)
         emb.set_author(name='NullCTF Help')
@@ -56,10 +62,12 @@ async def attach_embed_info(ctx=None, embed=None):
     embed.set_thumbnail(url=f'{bot.user.avatar_url}')
     return embed
 
+
 @bot.command()
 async def source(ctx):
     # Sends the github link of the bot.
     await ctx.send(help_info.src)
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -75,21 +83,24 @@ async def on_command_error(ctx, error):
         print("error not caught")
         print(error)
 
+
 @bot.command()
 async def request(ctx, feature):
     # Bot sends a dm to creator with the name of the user and their request.
-    creator = await bot.fetch_user(230827776637272064)
+    creator = await bot.fetch_user(133962599015383041)
     authors_name = str(ctx.author)
     await creator.send(f''':pencil: {authors_name}: {feature}''')
     await ctx.send(f''':pencil: Thanks, "{feature}" has been requested!''')
 
+
 @bot.command()
 async def report(ctx, error_report):
     # Bot sends a dm to creator with the name of the user and their report.
-    creator = await bot.fetch_user(230827776637272064)
+    creator = await bot.fetch_user(133962599015383041)
     authors_name = str(ctx.author)
     await creator.send(f''':triangular_flag_on_post: {authors_name}: {error_report}''')
     await ctx.send(f''':triangular_flag_on_post: Thanks for the help, "{error_report}" has been reported!''')
+
 
 @bot.command()
 async def amicool(ctx):
@@ -99,6 +110,47 @@ async def amicool(ctx):
     else:
         await ctx.send('lolno')
         await ctx.send('Psst, kid.  Want to be cool?  Find an issue and report it or request a feature!')
+
+
+def stringify_prefix_and_channel(flags_object):
+    pretty_prefixes = ''
+    pretty_ctf_channels = ''
+    for ctf in flags_object:
+        pretty_prefixes += f'"{ctf.flag_prefix}", '
+        pretty_ctf_channels += f'{ctf.channel.mention}, '
+    if pretty_prefixes:
+        pretty_prefixes = pretty_prefixes[:-2]
+    if pretty_ctf_channels:
+        pretty_ctf_channels = pretty_ctf_channels[:-2]
+    return (pretty_prefixes, pretty_ctf_channels)
+
+
+def get_flags(message):
+    flags = {}
+    server = config_vars.teamdb[str(message.guild.id)]
+    for ctf in server.find():
+        if 'flag_prefix' in ctf:
+            print(ctf)
+            regex = ctf['flag_prefix'] + '{.{6,}}'
+            found_flags = re.findall(regex, message.content)
+            if found_flags:
+                ctf_key
+                flags[ctf] = found_flags
+    return flags
+
+
+@bot.event
+async def on_message(message):
+    print("TEST")
+    flags = get_flags(message)
+    if flags:
+        await message.delete()
+        pretty_prefixes, pretty_ctf_channels = stringify_prefix_and_channel(
+            flags)
+        await message.channel.send(
+            content=f'```Message removed - Active CTF flag-prefix detected\nPlease only post flags with prefixes {pretty_prefixes} in {pretty_ctf_channels} respectively```')
+    else:
+        await bot.process_commands(message)
 
 if __name__ == '__main__':
     sys.path.insert(1, os.getcwd() + '/cogs/')
